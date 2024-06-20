b3e5ef363e158718a3ffe0a2f5869b1daf6ee2b1
carlosperales95 <carlos.perales95@opendeusto.es>
2024-06-20 20:48:25 +0200

    realized dist is not needed for this

--- dist/assets/artic-C-Y-h95q.js
+++
@@ -1,6 +0,0 @@
-const s=`# Hello World
-
-this is my devlog and now I can use markdown for articles and posts here
-
-### it supports
-most of the titles and [links](https://github.com/carlosperales95)`;export{s as default};
